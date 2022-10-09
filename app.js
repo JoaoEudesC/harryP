99ceddb095fe9f8e1479767c418d1380de3d5fb6
joaoeudes2501 <joaoeudes2501200@gmail.com>
2022-10-09 02:05:54 +0100

    alteraçao

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var pombo = document.getElementById("pombo")
+
+    pombo.addEventListener("click", capturar)
+
+function capturar(){
+    alert("sorte de principiante, voce acaba de capturar o pombo de ouro")
+
+}
+
+
+
+
+var mapa = document.getElementById("mapa")
+
+mapa.addEventListener("click", maroto)
+
+function maroto(){
+    mapa.innerHTML="isto é contra as regras"
+
+}
+
+
+
+
+
+
+
+
+
